36708a7878d1b96f76afd846eb45dffdd2ad33f2
borgeha <borge@10.24.22.103>
2020-01-06 14:55:23 +0100

    Utvidelse...

--- foreksempel/src/uke2/Smoothie.java
+++ foreksempel/src/uke2/Smoothie.java
@@ -9,6 +9,9 @@ public class Smoothie {
 		
 		double antall = f_1.getAntall()+f_2.getAntall()+f_3.getAntall();
 		System.out.println("Vi har en smoothie med så "+antall+" ting inni.");
+		System.out.println("Ser dere forresten at det som er inni main() i Frukt aldri blir kjørt?");
+		System.out.println("I stedet er det denne som kjøres.");
+		System.out.println("Som dere ser kan en faktisk ha en klasse som bare har en main-metode.");
 	}
 
 }
