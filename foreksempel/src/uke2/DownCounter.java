3ce24a4db8ae9297d942dfd42fbeccbc60d96858
borgeha <borgeha@NTNU18892.win.ntnu.no>
2020-01-07 15:24:07 +0100

    Oppdatert DownCOunter

--- foreksempel/src/uke2/DownCounter.java
+++ foreksempel/src/uke2/DownCounter.java
@@ -4,15 +4,32 @@ public class DownCounter {
 
 	int counter = 0;
 
-	
-	public int getCounter() {
-		return counter;
+	public boolean isFinished() {
+		return counter == 0;
 	}
-
-
+	
+	public void countDown() {
+		if (! isFinished()) {
+			counter -= 1;
+			System.out.println("Teller ned, er på "+counter);
+		}
+	}
+	
+	
 	public static void main(String[] args) {	
 		DownCounter dc = new DownCounter(4);
-		System.out.println(dc.getCounter());
+		System.out.println(dc.isFinished());
+		dc.countDown();
+		System.out.println(dc.isFinished());
+		dc.countDown();
+		System.out.println(dc.isFinished());
+		dc.countDown();
+		System.out.println(dc.isFinished());
+		dc.countDown();
+		System.out.println(dc.isFinished());
+		dc.countDown();
+		System.out.println(dc.isFinished());
+		dc.countDown();
 
 	}
 
