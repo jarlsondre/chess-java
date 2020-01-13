ccfcf9906aa8e825be12c0da6d52f1aadd35ccd6
borgeha <borgeha@NTNU18892.win.ntnu.no>
2020-01-13 14:18:50 +0100

    Denne

---
+++ foreksempel/src/uke3/Regne.java
@@ -0,0 +1,30 @@
+package uke3;
+
+public class Regne {
+
+	int a, b;
+	
+	
+	
+	public Regne(int aa, int b) {
+		super();
+		a = aa;
+		this.b = b;
+	}
+
+
+	public int gang() {
+		return a*b;
+	}
+
+	public int pluss() {
+		return a+b;		
+	}
+	
+	public static void main(String[] args) {
+		Regne r = new Regne(3,5);
+		System.out.println(r.gang());
+		System.out.println(r.pluss());
+	}
+
+}
