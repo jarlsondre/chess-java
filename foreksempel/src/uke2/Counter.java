4b93f47445c530d7075674eb5767cc567854b52a
borgeha <borgeha@NTNU18892.win.ntnu.no>
2020-01-26 16:53:38 +0100

    Fikset feoil

---
+++ foreksempel/src/uke2/Counter.java
@@ -0,0 +1,45 @@
+package uke2;
+
+public class Counter {
+
+	int end;
+	int counter = 1;
+	
+	public Counter(int i) {
+		end = i;
+	}
+	
+	boolean count() {
+		if (counter == end)
+			return true;
+		else {
+			counter++;
+			return false;
+		}
+	}
+	
+	boolean count(int i) {
+		if (counter == end)
+			return true;
+		else {
+			counter += i;
+			return false;
+		}
+	}
+
+
+	public static void main(String[] args) {
+
+		Counter c = new Counter(3);
+		System.out.println(c.getCounter());
+		System.out.println(c.count(2));
+		System.out.println(c.getCounter());
+		System.out.println(c.count(2));
+	}
+
+	private int getCounter() {
+		// TODO Auto-generated method stub
+		return this.counter;
+	}
+
+}
