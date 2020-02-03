7497a216d6ea535011dc82ea7615984399057c23
borgeha <borgeha@10.53.49.164>
2020-02-03 11:48:06 +0100

    En bok. En liiiiten bok.

---
+++ foreksempel/src/uke6/arv/Bok.java
@@ -0,0 +1,26 @@
+package uke6.arv;
+
+public class Bok {
+
+	String tittel;
+	String forfatter;
+	int sideantall;
+
+	public Bok(String tittel, String forfatter, int sideantall) {
+		super();
+		this.tittel = tittel;
+		this.forfatter = forfatter;
+		this.sideantall = sideantall;
+	}
+
+	public static void main(String[] args) {
+		Bok bok = new Bok("Das Kapital","Karl Marx",500);
+		System.out.println(bok);
+	}
+
+	@Override
+	public String toString() {
+		return "Bok [tittel=" + tittel + ", forfatter=" + forfatter + ", sideantall=" + sideantall + "]";
+	}
+
+}
