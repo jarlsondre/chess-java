1428e77a0809f5302fd9b85688c32cf476b2b251
zootos <halvard.hummel@hotmail.com>
2020-02-09 16:51:38 +0100

    Exercises for colloquium 1

---
+++ foreksempel/src/kollokvie1/underveis/Game.java
@@ -0,0 +1,49 @@
+package kollokvie1.underveis;
+
+public class Game {
+
+	private Tile[][] board;
+
+	public Game(int width, int height) {
+		board = new Tile[height][width];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				board[y][x] = new Tile(x, y);
+			}
+		}
+	}
+
+	public boolean isTile(int x, int y) {
+		return 0 <= x && x < getWidth() && 0 <= y && y < getHeight();
+	}
+
+	public Tile getTile(int x, int y) {
+		if (!isTile(x, y)) {
+			throw new IllegalArgumentException("Not a valid tile");
+		}
+
+		return board[y][x];
+	}
+
+	public int getHeight() {
+		return board.length;
+	}
+
+	public int getWidth() {
+		return board[0].length;
+	}
+
+	@Override
+	public String toString() {
+		String representation = "";
+
+		for (int y = 0; y < getHeight(); y++) {
+			for (int x = 0; x < getWidth(); x++) {
+				representation = representation + getTile(x, y).toString();
+			}
+			representation = representation + "\n";
+		}
+		return representation;
+	}
+
+}
