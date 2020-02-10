46fa5a357efe66ec53cd155f7f3e989bfaa0ab08
borgeha <borgeha@10.24.23.7>
2020-02-10 11:31:57 +0100

    Basis for forelesninger denne uken.

---
+++ foreksempel/src/uke7/folder/Folder.java
@@ -0,0 +1,29 @@
+package uke7.folder;
+
+public class Folder {
+	
+/*
+ * "Etter denne timen kommer det til å holdes frivillig kollokvietime i øving 4 på R2 som 
+ * dere finner direkte til venstre når dere går ut fra forelesningen. Her kommer vi til å 
+ * jobbe videre på og fullføre logikken til Snakebird-spillet som vi startet på i 
+ * øvingsforelesningen forrige uke. Det kommer til å være vitasser i tillegg til andre fra 
+ * fagstaben tilstede som hjelper dere med oppgaveløsningen og det varer fra 14:15-16:00" 	
+ */
+	public static void main(String[] args) {
+//		Folder root = new Folder("Rot",null);
+//		Folder users = new Folder("users",root);
+//		Folder borgeh = new Folder("borgeh",users);
+//		File rotfil = new File("rotfil.txt",root);
+//		Folder tmp = new Folder("tmp",root);
+//		File tmpfil = new File("tmpfil.txt",tmp);
+//		File egenfil1 = new File("egenfil.txt", borgeh);
+//		File egenfil2 = new File("egenfil2.txt", borgeh);
+//		Folder div = new Folder("div",root);
+//		root.printContent();
+//		users.move(borgeh);
+//		root.printContent();
+
+	}
+
+
+}
