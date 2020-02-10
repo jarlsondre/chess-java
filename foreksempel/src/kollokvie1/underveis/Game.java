3b1962e33250b4a1a58621f8a578cbf3f9faa32e
zootos <halvard.hummel@hotmail.com>
2020-02-10 14:32:58 +0100

    Oppgave 1 fra kollokvie

--- foreksempel/src/kollokvie1/underveis/Game.java
+++ foreksempel/src/kollokvie1/underveis/Game.java
@@ -1,8 +1,13 @@
 package kollokvie1.underveis;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
 
 	private Tile[][] board;
+	private List<Tile> snake;
 
 	public Game(int width, int height) {
 		board = new Tile[height][width];
@@ -11,6 +16,18 @@ public class Game {
 				board[y][x] = new Tile(x, y);
 			}
 		}
+	}
+	
+	public void createSnake(List<Tile> snake) {
+		if (this.snake != null) {
+			throw new IllegalStateException("Snake already created");
+		}
+		
+		for (Tile tile : snake) {
+			tile.setSnake();
+		}
+		
+		this.snake = new ArrayList<>(snake);
 	}
 
 	public boolean isTile(int x, int y) {
@@ -44,6 +61,32 @@ public class Game {
 			representation = representation + "\n";
 		}
 		return representation;
+	}
+	
+	public static void main(String[] args) {
+	    Game game = new Game(16, 12);
+	    game.getTile(6, 11).setGround();
+	    game.getTile(7, 11).setGround();
+	    game.getTile(8, 11).setGround();
+	    game.getTile(6, 10).setGround();
+	    game.getTile(7, 10).setGround();
+	    game.getTile(8, 10).setGround();
+	    game.getTile(9, 10).setGround();
+	    game.getTile(6, 9).setGround();
+	    game.getTile(7, 9).setGround();
+	    game.getTile(8, 9).setGround();
+	    game.getTile(9, 9).setGround();
+	    game.getTile(6, 8).setGround();
+	    game.getTile(8, 6).setGround();
+	    game.getTile(10, 6).setGround();
+	    game.getTile(5, 5).setGround();
+	    game.getTile(5, 6).setFruit();
+	    game.getTile(9, 6).setFruit();
+	    game.getTile(8, 3).setGoal();
+
+	    game.createSnake(Arrays.asList(game.getTile(9, 8), game.getTile(8, 8)));
+
+	    System.out.println(game);
 	}
 
 }
