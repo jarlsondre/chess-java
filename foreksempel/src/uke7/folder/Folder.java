912941ba43bb428f829757340a13033855cac9be
borgeha <borgeha@10.24.23.7>
2020-02-10 13:00:08 +0100

    Første time

--- foreksempel/src/uke7/folder/Folder.java
+++ foreksempel/src/uke7/folder/Folder.java
@@ -1,28 +1,81 @@
 package uke7.folder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
 	
-/*
- * "Etter denne timen kommer det til å holdes frivillig kollokvietime i øving 4 på R2 som 
- * dere finner direkte til venstre når dere går ut fra forelesningen. Her kommer vi til å 
- * jobbe videre på og fullføre logikken til Snakebird-spillet som vi startet på i 
- * øvingsforelesningen forrige uke. Det kommer til å være vitasser i tillegg til andre fra 
- * fagstaben tilstede som hjelper dere med oppgaveløsningen og det varer fra 14:15-16:00." 	
- */
+	String name;
+	Folder parent;
+	List<Folder> folders;
+	// List<File> files;
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-//		Folder root = new Folder("Rot",null);
-//		Folder users = new Folder("users",root);
-//		Folder borgeh = new Folder("borgeh",users);
-//		File rotfil = new File("rotfil.txt",root);
-//		Folder tmp = new Folder("tmp",root);
-//		File tmpfil = new File("tmpfil.txt",tmp);
+		Folder home = new Folder("home",null);
+		Folder borgeh = new Folder("borgeh",home);
+		Folder hal = new Folder("hal",home);
+		Folder div = new Folder("div",borgeh);
+//		System.out.println(home);
+//		System.out.println(borgeh);
+//		System.out.println(div);
+//		System.out.println(hal);
+		home.printTree();
+//		File homefil = new File("tmpfil.txt",home);
 //		File egenfil1 = new File("egenfil.txt", borgeh);
 //		File egenfil2 = new File("egenfil2.txt", borgeh);
-//		Folder div = new Folder("div",root);
 //		root.printContent();
 //		users.move(borgeh);
 //		root.printContent();
 
+	}
+	
+	private void printTree() {
+		System.out.println(this);
+		
+		for (Folder folder : folders) {
+			folder.printTree();
+		}
+		
+	}
+	@Override
+	public String toString() {
+		String tmp = "/" + name;
+		if (parent != null)
+			tmp = parent.toString() + tmp;
+		return tmp;
+	}
+	public Folder(String name, Folder parent) {
+		super();
+		folders = new ArrayList<>();
+//		files = new ArrayList<>();
+		this.name = name;
+		this.parent = parent;
+		if (parent != null) {
+			setParent(parent);
+			parent.addFolder(this);
+		}
+		
+	}
+	private void addFolder(Folder folder) {
+		folders.add(folder);
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Folder getParent() {
+		return parent;
+	}
+	public void setParent(Folder parent) {
+		this.parent = parent;
 	}
 
 
