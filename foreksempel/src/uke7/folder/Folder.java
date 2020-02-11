3feea9e825b5818bc69c5733cd007366e4de88fc
borgeha <borgeha@10.24.23.7>
2020-02-11 14:09:25 +0100

    Før forelesning - lagt til contains2 som er elendig i forhold til contains, men som krever kobling til parent.

--- foreksempel/src/uke7/folder/Folder.java
+++ foreksempel/src/uke7/folder/Folder.java
@@ -4,31 +4,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Folder {
-	
+
 	String name;
 	Folder parent;
 	List<Folder> folders;
 	// List<File> files;
-	
+
 	public static void main(String[] args) {
 		Folder home = new Folder("home",null);
 		Folder borgeh = new Folder("borgeh",home);
 		Folder hal = new Folder("hal",home);
 		Folder div = new Folder("div",borgeh);
 		borgeh.setName("kari");
+		System.out.println("borgeh i hal: "+hal.contains2(borgeh));
 		borgeh.move(hal);
 		borgeh.printTree();
-		
+		System.out.println("borgeh i hal: "+hal.contains2(borgeh));
+
+		// Litt kode for å vise hvordan contains2 er elendig i forhold til contains.
+		// Vi lager tretti nye foldere inni home.
+		for (int i = 0; i < 30; i++) {
+			home.addFolder(new Folder(Integer.toString(i),home));				
+		}
+
+		// Inni hver av home sine subfoldere lager vi tretti nye foldere.
+		for (Folder folder : home.folders) {
+			for (int i = 0; i < 30; i++) {
+				folder.addFolder(new Folder(Integer.toString(i),folder));				
+			}
+		}
+
+		//		home.printTree();
+
+		// Se på forskjellen i hva som skrives ut av disse - hvilken er mest effektiv?
+		//		home.contains2(home.folders.get(25));
+		System.out.println("contains:");
+		Folder destination = home.folders.get(23).folders.get(24);
+		System.out.println(destination);
+		System.out.println(home.contains(destination));
+		//		versus home.contains2(destination);
+
 		//		File homefil = new File("tmpfil.txt",home);
-//		File egenfil1 = new File("egenfil.txt", borgeh);
-//		File egenfil2 = new File("egenfil2.txt", borgeh);
-//		root.printContent();
-//		users.move(borgeh);
-//		root.printContent();
+		//		File egenfil1 = new File("egenfil.txt", borgeh);
+		//		File egenfil2 = new File("egenfil2.txt", borgeh);
 
 	}
-	
-	
+
+
+	/* 
+	 * Dette er den 'enkleste' måten å tenke seg at en sjekker om
+	 * destination er en subfolder av this. Problemet er at denne sjekker
+	 * potensielt ALLE subfoldere til ALLE subfoldere. Det kan bli mye!
+	 * contains sjekker motsatt vei i 1:n-relasjonen mellom en folder
+	 * og dens subfoldere, da er det kun 1 objekt per nivå, og ikke n.
+	 */
+	boolean contains2(Folder destination) {
+//		System.out.println("Sjekker mot "+destination.toString());
+		if (this == destination)
+			return true;
+		for (Folder folder : folders) {
+			System.out.println("Sjekker så mot "+folder.toString());
+			if (folder.contains2(destination))
+				return true;
+		}		
+		return false;
+	}
+
 	/*
 	 *  Hvordan fungerer contains?
 	 *  Hvis en skal sjekke om this inneholder destination, så
@@ -41,6 +82,8 @@ public class Folder {
 	 *  returnerer en false - destination er ikke en subfolder.
 	 */
 	boolean contains(Folder destination) {
+		if (destination != null)
+			System.out.println("Sjekker mot " + destination.toString());
 		if (destination == this) {
 			return true;
 		}
@@ -49,23 +92,23 @@ public class Folder {
 		}
 		else return this.contains(destination.parent);
 	}
-	
+
 	private void move(Folder destination) {
 		System.out.println("Skal flytte " + this.getName() + 
 				" til " + destination.getName());
 		if (destination != null && this.contains(destination)) {
 			throw new IllegalStateException("Uendelig løkke...");
 		}
-		
+
 		// parent må ha beskjed før vi endrer parent til en ny Folder.
 		parent.removeFolder(this);
-		
+
 		// Selv
 		this.parent = destination;
-		
+
 		// ny parent må få beskjed
 		parent.addFolder(this);
-		
+
 	}
 
 	private void removeFolder(Folder folder) {
@@ -74,12 +117,13 @@ public class Folder {
 
 	private void printTree() {
 		System.out.println(this);
-		
+
 		for (Folder folder : folders) {
 			folder.printTree();
 		}
-		
+
 	}
+
 	@Override
 	public String toString() {
 		String tmp = "/" + name;
@@ -90,20 +134,21 @@ public class Folder {
 	public Folder(String name, Folder parent) {
 		super();
 		folders = new ArrayList<>();
-//		files = new ArrayList<>();
+		//		files = new ArrayList<>();
 		this.name = name;
-//		this.parent = parent; // Denne strykes, brukes igjen etterpå.
+		//		this.parent = parent; // Denne strykes, brukes igjen etterpå.
 		if (parent != null) {
 			setParent(parent);
 			parent.addFolder(this);
 		}
-		
+
 	}
 	void addFolder(Folder folder) {
-		folders.add(folder);
+		if (!folders.contains(folder)) // Lagt til denne etter forelesning!
+			folders.add(folder);
 	}
-		
-	
+
+
 	public String getName() {
 		return name;
 	}
@@ -116,7 +161,7 @@ public class Folder {
 			throw new IllegalStateException("Name cannot be of zero length.");
 		this.name = name;
 	}
-	
+
 	public Folder getParent() {
 		return parent;
 	}
