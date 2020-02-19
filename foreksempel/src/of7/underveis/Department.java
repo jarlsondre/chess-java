5f0c54230eb13ab5001712777123a374e77f2c3a
zootos <halvard.hummel@hotmail.com>
2020-02-19 17:31:24 +0100

    Solution for exercise 1 from exercise lecture 7

--- foreksempel/src/of7/underveis/Department.java
+++ foreksempel/src/of7/underveis/Department.java
@@ -2,4 +2,14 @@ package of7.underveis;
 
 public class Department {
 
+	private Department superDepartment;
+	
+	public Department() {
+		
+	}
+	
+	public Department(Department superDepartment) {
+		this.superDepartment = superDepartment;
+	}
+	
 }
