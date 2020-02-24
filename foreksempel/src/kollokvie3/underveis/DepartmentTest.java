89aacf9e400b8dc5e24e037ed014ad00b931e2bc
zootos <halvard.hummel@hotmail.com>
2020-02-24 14:58:04 +0100

    Exercise 2 from colloquium 3

--- foreksempel/src/kollokvie3/underveis/DepartmentTest.java
+++ foreksempel/src/kollokvie3/underveis/DepartmentTest.java
@@ -1,7 +1,9 @@
 package kollokvie3.underveis;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -16,5 +18,21 @@ public class DepartmentTest {
 		assertEquals(department1, department2.getSuperDepartment());
 	}
 	
+	@Test
+	public void testContains() {
+		Department department1 = new Department();
+		Department department2 = new Department(department1);
+		
+		assertTrue(department1.contains(department2));
+		
+		Department department3 = new Department(department2);
+		
+		assertTrue(department1.contains(department3));
+		
+		Department department4 = new Department();
+		
+		assertFalse(department1.contains(department4));
+		assertFalse(department3.contains(department1));
+	}
 	
 }
