e0205ea7b3ceb9fe214d116b156d8b922fce3256
zootos <halvard.hummel@hotmail.com>
2020-02-26 18:46:35 +0100

    Solutions from exercise lecture 8

---
+++ foreksempel/src/of8/lf/Animal.java
@@ -0,0 +1,9 @@
+package of8.lf;
+
+public interface Animal {
+
+	String getName();
+	int getAge();
+	String makeSound();
+	
+}
---
+++ foreksempel/src/of8/lf/Dog.java
@@ -0,0 +1,28 @@
+package of8.lf;
+
+public class Dog implements Animal {
+
+	private String name;
+	private int age;
+	
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String makeSound() {
+		return name + " says Woof!";
+	}
+
+}
