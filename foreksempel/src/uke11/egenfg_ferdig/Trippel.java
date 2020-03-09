33d1eb52bad5eeb51fcf0fe9123a83894bf2cee8
borgeha <borgeha@10.24.4.220>
2020-03-09 10:31:50 +0100

    Før forelesning

---
+++ foreksempel/src/uke11/egenfg_ferdig/Trippel.java
@@ -0,0 +1,15 @@
+package uke11.egenfg_ferdig;
+
+
+/* 
+ * Et funksjonelt grensesnitt (FG) er et grensesnitt som har EN abstrakt metode - den skal
+ * altså ikke være implementert i grensesnittet men kun nevnt ved overskrift.
+ * Vi har allerede en drøss med slike FG, men ingen av dem som tar inn 3 variable. 
+ * Dette skal vi nå lage! 
+ */
+public interface Trippel {
+
+	// Det eneste vi kan gjøre her. Det er opp til implementasjonen å kverne på tallene.
+	public abstract int beregn(int a, int b, int c);
+	
+}
