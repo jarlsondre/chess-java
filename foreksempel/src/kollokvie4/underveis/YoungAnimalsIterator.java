9214f69b62ae8d1e360f27bfee584ef5f2ea7ff8
zootos <halvard.hummel@hotmail.com>
2020-03-09 14:40:17 +0100

    Solutions for exercise 1 in colloquium 4

---
+++ foreksempel/src/kollokvie4/underveis/YoungAnimalsIterator.java
@@ -0,0 +1,38 @@
+package kollokvie4.underveis;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class YoungAnimalsIterator implements Iterator<Animal> {
+
+	private Farm farm;
+	private int index;
+	
+	public YoungAnimalsIterator(Farm farm) {
+		this.farm = farm;
+	}
+
+	@Override
+	public boolean hasNext() {
+		while (index < farm.numberOfAnimals()) {
+			if (farm.getAnimal(index).getAge() < 2) {
+				return true;
+			}
+			
+			index++;
+		}
+		
+		return false;
+		
+		//return farm.numberOfAnimals() > index;
+	}
+
+	@Override
+	public Animal next() {
+		if (!hasNext()) {
+			throw new NoSuchElementException("There is no next element");
+		}
+		return farm.getAnimal(index++);
+	}
+	
+}
