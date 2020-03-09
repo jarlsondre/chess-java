5ee3d569430fa01ad2c3f75fb65375c153cb50ff
zootos <halvard.hummel@hotmail.com>
2020-03-09 15:35:42 +0100

    Solutions from exercise 4 in colloquium 4

--- foreksempel/src/kollokvie4/underveis/Farm.java
+++ foreksempel/src/kollokvie4/underveis/Farm.java
@@ -1,6 +1,8 @@
 package kollokvie4.underveis;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -33,15 +35,13 @@ public class Farm implements Iterable<Animal> {
 	}
 	
 	public static void main(String[] args) {
-		Farm farm = new Farm();
-		farm.addAnimal(new Dog("Ludo", 12));
-		farm.addAnimal(new Dog("Fido", 0));
-		farm.addAnimal(new Chicken("Peter", 1));
-		farm.addAnimal(new Chicken("Albert", 4));
+		List<Animal> animals = Arrays.asList(new Dog("Ludo", 12), new Dog("Fido", 0), new Chicken("Peter", 1),
+				new Chicken("Albert", 4));
 		
-		Iterator<Animal> iterator = new FilterAnimalsIterator(farm, new DogPredicate());
-		while (iterator.hasNext()) {
-			System.out.println(iterator.next().makeSound());
+		Collections.sort(animals, (animal1, animal2) -> animal1.getName().compareTo(animal2.getName()));
+		
+		for (Animal animal : animals) {
+			System.out.println(animal.makeSound());
 		}
 	}
 	
