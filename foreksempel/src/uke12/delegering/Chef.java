8f5222bb32602e0e68ea589a499fbc8128c920d6
borgeha <borgeha@DESKTOP-S62USNL>
2020-03-18 13:39:17 +0100

    Eksempel med kokker lag ut. Det kommer mer om unntak.

---
+++ foreksempel/src/uke12/delegering/Chef.java
@@ -0,0 +1,11 @@
+package uke12.delegering;
+
+
+/*
+ * Grensesnittet som enhver kokk på oppfylle.
+ */
+public interface Chef {
+	public String makeMain();
+	public String makePotatoes();
+	public String makeSauce();
+}
