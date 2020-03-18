0c1a5853a666af5423a27fbf99e178c06854327f
borgeha <borgeha@DESKTOP-S62USNL>
2020-03-18 13:40:55 +0100

    Kokkeeksempel. Unntak kommer.

--- foreksempel/src/uke12/delegering/Chef.java
+++ foreksempel/src/uke12/delegering/Chef.java
@@ -3,6 +3,7 @@ package uke12.delegering;
 
 /*
  * Grensesnittet som enhver kokk på oppfylle.
+ * Hovedkokken ChefdelaChef og underkokkene fyller alle denne rollen.
  */
 public interface Chef {
 	public String makeMain();
